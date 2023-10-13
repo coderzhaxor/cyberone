9d77b5daf4b23e46b2cca6faa48f30066e018743
coderzhaxor <ajanuantara@gmail.com>
2023-10-13 14:19:44 +0700

    Add Signup form

---
+++ tailwind.config.js
@@ -0,0 +1,32 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['register.html'],
+  theme: {
+    extend: {
+      colors: {
+        'dark': {
+          '100': '#2F2F2F',
+          '200': '#2A2A2A',
+          '300': '#2B2B2B',
+          '400': '#212121',
+          '500': '#1B1B1B'
+        },
+        'light': {
+          '100': '#FFFFFF',
+          '200': '#EBEBEB',
+          '300': '#474747',
+          '400': '#E3E3E3'
+        }
+      },
+      backgroundImage: {
+        'world-dark': "url('/assets/img/world-dark.png')",
+        'world-light': "url('/assets/img/world-light.png')"
+      },
+      fontFamily: {
+        'rajdhani': "'Rajdhani', sans-serif"
+      }
+    },
+  },
+  plugins: [],
+}
+
